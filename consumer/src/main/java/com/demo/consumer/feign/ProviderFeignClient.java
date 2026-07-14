ce0afc709837200a4ba4562295641b3355f8dd53
vigneshs1997 <vigneshmaster1997@gmail.com>
2026-07-14 11:50:19 +0530

    Open feign client is implemented

---
+++ consumer/src/main/java/com/demo/consumer/feign/ProviderFeignClient.java
@@ -0,0 +1,10 @@
+package com.demo.consumer.feign;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "provider-service", url = "http://localhost:8081")
+public interface ProviderFeignClient {
+
+    @GetMapping("/instance-info")
+    String getInstanceInfo();
+}
